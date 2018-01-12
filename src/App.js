aff2bad1dc5c898206c14e595809d5767bb95466
kritpongthanu <kritpongthnu_19@hotmail.com>
2018-01-12 09:37:46 +0700

    refactor & add interval

--- src/App.js
+++ src/App.js
@@ -4,34 +4,44 @@ import axios from 'axios'
 import _ from 'lodash'
 
 class App extends Component {
+  SYMBOLS = ['OMG', 'BTC', 'XRP']
+
   constructor(props){
     super(props)
     this.state = {}
   }
 
-  componentDidMount(){
+  //refactor
+  extractPrice(data){
+    let price_state = {}
+    _.each(this.SYMBOLS, symbol => {
+      price_state[symbol] = _.find(data, ['symbol', symbol]).price_thb
+    })
+    this.setState(price_state)
+  }
+
+  loadPrice(){
     let self = this
     axios.get('https://api.coinmarketcap.com/v1/ticker/?convert=THB')
     .then(function (response) {
-      let price_state = {}
-      _.each(self.SYMBOLS, symbol =>{
-        price_state[symbol] = _.find(response.data, ['symbol', symbol]).price_thb
-      })
-    self.setState({
-      BTC: btc.price_thb,
-      OMG: omg.price_thb
-    })
+      self.extractPrice(response.data)
     })
     .catch(function (error) {
       console.log(error);
     });
   }
 
+  componentDidMount(){
+    setInterval(this.loadPrice.bind(this), 3000)
+  }
+
   render() {
     return (
-    <div>
-      _.map(this.SYMBOLS, symbol => <Ticker symbol={symbol} price={this.state[symbol]}/>)
-              </div>
+      <div>
+        {
+          _.map(this.SYMBOLS, symbol => <Ticker symbol={symbol} price={this.state[symbol]}/>)
+        }
+      </div>
     );
   }
 }
